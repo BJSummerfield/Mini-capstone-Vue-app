9ff7f44bea12d65ef4709615ed7897654e7baf18
Brian Summerfield <brianjsummerfield@gmail.com>
2019-04-04 21:04:11 -0500

    add crud views to home.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,18 +5,30 @@
     <p>price:<input type="text" v-model="newProductPrice"></p>
     <p>Description: <input type="text" v-model="newProductDescription"></p>
     <p>Image_url:<input type="text" v-model="newProductImage_url"></p>
-    <button v-on:click="addNewProduct()">add a new product</button> 
-    
+    <button v-on:click="addNewProduct()">Add a new product</button> 
     <p></p>
     <hr>
+    
     <div v-for="product in products">
-    <img v-bind:src="product.image_url">
-    <p>{{ product.name }}</p>
-    <p>{{ product.description }}</p>
-    <p>{{ product.price }}</p>
-    <hr>
-    <!-- <p>{{ product.image.url }}</p> -->
-  </div>
+      <img v-bind:src="product.image_url">
+      <p><button v-on:click="toggleInfo(product)">More Info</button>
+        </p>
+      <p>{{ product.name }}</p>
+      <p>{{ product.formatted['price'] }}</p>
+      <div v-if="product === currentProduct">
+        <p>{{ product.description }}</p>
+        <p>Tax: {{ product.formatted['tax'] }}</p>
+        <p>Total: {{ product.formatted['total'] }}</p>
+        <p>name: <input type="text" v-model="product.name"></p>
+        <p>description: <input type="text" v-model="product.description"></p>
+        <p>price: <input type="text" v-model="product.price"></p>
+        <p>image_url: <input type="text" v-model="product.image_url"></p>
+         <button v-on:click="updateProduct(product)">Update Product</button>
+         <button v-on:click="deleteProduct(product)">Delete Product</button>
+
+      </div>
+      <hr>
+    </div>
   </div>
 </template>
 
@@ -36,8 +48,8 @@ export default {
       newProductName: "",
       newProductPrice: "",
       newProductDescription: "",
-      newProductImage_url: ""
-
+      newProductImage_url: "",
+      currentProduct: []
     };
   },
   created: function() {
@@ -59,9 +71,42 @@ export default {
       axios.post("/api/products", params).then(response => {
         console.log(response.data);
         this.products.push(response.data);
-
+      });
+    },
+    toggleInfo: function(theProduct) {
+      if (this.currentProduct === theProduct) {
+        this.currentProduct = {};
+      } else { 
+        this.currentProduct = theProduct;
+      }
+    },
+    updateProduct: function(theProduct) {
+      console.log('updating the product...');
+      var params = {
+        name: theProduct.name,
+        price: theProduct.price,
+        description: theProduct.description,
+        image_url: theProduct.image_url
+      };
+      axios.patch("/api/products/" + theProduct.id, params).then(response => { 
+        console.log(response);
+        theProduct = response.data;
+      });
+    },
+    deleteProduct: function(theProduct) {
+      console.log('deleting product');
+      var params = {
+        id: theProduct.id
+      };
+      var index = this.products.indexOf(theProduct);
+      console.log(index);
+      this.products.splice(index, 1);
+      
+      axios.delete("/api/products/" + theProduct.id, params).then(response => {
+        console.log(response);
+        theProduct = response.data;
       });
     }
   }
 };
-</script>
+</script>    
